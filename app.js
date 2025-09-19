930f62c8447f7185ba57900b413b26e4fb787029
Cristian <cristian.sebamorales@gmail.com>
2025-09-19 12:52:41 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+const {createProxyMiddleware} = require("http-proxy-middleware");
+
+
+// Proxy: de /api hacia tu servidor HTTP
+app.use("/", createProxyMiddleware({
+  target: "http://api.razycia.net:11444/RazAPI", // tu backend HTTP
+  changeOrigin: true,
+}));
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Express server running at http://localhost:${PORT}/`);
+});
