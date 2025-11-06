012e9b6d98a148aaba3f9d49bb9883ebe7e66f6f
Cristian <cristian.sebamorales@gmail.com>
2025-11-06 20:38:31 -0300

    change basepath

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const {createProxyMiddleware} = require("http-proxy-middleware");
 
 // Proxy: de /api hacia tu servidor HTTP
 app.use("/", createProxyMiddleware({
-  target: "http://api.razycia.net:11444", // tu backend HTTP
+  target: "http://comprobantes.razycia.net:8088", // tu backend HTTP
   changeOrigin: true,
 }));
 
